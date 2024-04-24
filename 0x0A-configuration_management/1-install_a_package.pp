2e812f7bf1666d9bf6e93339fb6c9ad8c215adba
Zerufael Aemero <zerufaelaemero@gmail.com>
2024-04-24 11:21:41 -0700

    edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#Install flask / Version must be 2.1.0
+# Using Puppet, install flask from pip3.
 
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
-} 
+    ensure   => '2.1.0',
+    provider => 'pip3',
+}
