299f86f7c02be9753df8c5286883e4407d2d54c8
Zerufael Aemero <zerufaelaemero@gmail.com>
2024-04-24 10:04:49 -0700

    edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
 #Install flask / Version must be 2.1.0
-exec { 'install_flask_2.1.0':
- command => 'pip3 install Flask==2.1.0',
- path    => ['/usr/bin/'],
- unless => 'pip3 show Flask | grep "Version: 2.1.0"',
-}
-  
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+} 
