684918c64e5f62ca306729adc279f1c073537032
zerufael <zerufaelaemero@gmail.com>
2024-05-22 13:53:49 -0700

    edit

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,32 +5,14 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    # Construct URLs for todos and users
     user_id = sys.argv[1]
-    url_todos = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
-    url_users = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    # Fetch data from API
-    todos = requests.get(url_todos)
-    users = requests.get(url_users)
-
-    # Check if requests are successful
-    if todos.status_code == 200 and users.status_code == 200:
-
-        # Parse JSON responses
-        todo = todos.json()
-        user = users.json()
-
-        # Specify the path to save the CSV file
-        csv_file = "USER_ID.csv"
-
-        # Define username
-        username = user.get("username")
-
-        # Write JSON data to CSV file
-        with open(csv_file, mode='w', newline='') as file:
-            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-            [writer.writerow(
-                [user_id, username, t.get("completed"), t.get("title")]
-            ) for t in todo]
-
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
